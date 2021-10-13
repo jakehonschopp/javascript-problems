038a9b143b46d9dfe24b5ec96dec92680c0f7515
Jake Honschopp <Jakehonschopp@yahoo.com>
2021-10-13 17:29:58 -0400

    fixing issues with console logging compared to returning value of true or false with the palindrome status

--- functions-problems/jsp4s.js
+++ functions-problems/jsp4s.js
@@ -7,12 +7,14 @@ function palindromeOrNot(string) {
   const reverseString = reverseArrayValues.join("");
   if(string == reverseString) {
     console.log("It is a palindrome");
+    return true; 
     }
   else {
     console.log("It is not a palindrome");
+    return false;
   }
   
-  return palindromeOrNot;
+  
 }
 
 console.log(palindromeOrNot("racecar"));
@@ -22,6 +24,6 @@ console.log(palindromeOrNot("racecar"));
    Step 3: reverse your new array and call this new variable "reverseArrayValues"
    Step 4: join the new array into a new string, and call this "reverseString"
    Step 5: use an if else statement to see if string is equal to reverseString
-   Step 6: return the function palindromeOrNot
-   Step 7: log the value of "racecar" to the console, and return "It is a palindrome"
+   Step 6: return "true" if the string is a palindrome, and return "false" if it is not.
+   Step 7: log the value of "racecar" to the console, and return "true".
 */ 
